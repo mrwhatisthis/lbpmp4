3e296968f37cbfc20aeccb6e10400a3f9f9f7e06
mrwhatisthis <mrwhatisthis@gmail.com>
2024-01-21 18:59:39 +0200

    first commit

--- lib/weather.dart
+++ lib/weather.dart
@@ -2,31 +2,49 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import 'db.dart';
+
+
 Future<WeatherInfo> fetchWeather(String city) async {
-  final response = await http.get(Uri.parse("https://goweather.herokuapp.com/weather/$city"));
+  try {
+    final response = await http
+        .get(Uri.parse("https://goweather.herokuapp.com/weather/$city"));
 
-  if (response.statusCode == 200) {
+    if (response.statusCode == 200) {
+      final w = WeatherInfo.fromJson(
+          city, jsonDecode(response.body) as Map<String, dynamic>);
 
-    return WeatherInfo.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-  } else {
-    throw Exception("Failed to load WeatherInfo. Status code: ${response.statusCode}, response: ${response.body}.");
+      // Save to DB.
+      await insertWeather(w);
+
+      return w;
+    } else {
+      final w = await getWeather(city);
+      return w;
+    }
+  } on Exception catch (e) {
+    final w = await getWeather(city);
+    return w;
   }
+
 }
 
 
-class WeatherInfo {
 
+class WeatherInfo {
+  final String id;
   final String temperature;
   final String wind;
   final String description;
 
   const WeatherInfo({
+    required this.id,
     required this.temperature,
     required this.wind,
     required this.description,
   });
 
-  factory WeatherInfo.fromJson(Map<String, dynamic> json) {
+  factory WeatherInfo.fromJson(String id, Map<String, dynamic> json) {
     return switch(json) {
       {
       "temperature": String temperature,
@@ -34,6 +52,7 @@ class WeatherInfo {
       "description": String description
       } =>
           WeatherInfo(
+            id: id,
             temperature: temperature,
             wind: wind,
             description: description,
@@ -41,6 +60,16 @@ class WeatherInfo {
       _ =>
       throw const FormatException(
           "Failed to load JSON response for WeatherInfo."),
+    };
+  }
+
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'temperature': temperature,
+      'wind': wind,
+      'description': description
     };
   }
 
